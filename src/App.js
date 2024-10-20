5e3b710624db7706178eda88daa5547c40d6bb26
Kohi <95735459+KOOHIIii@users.noreply.github.com>
2024-10-20 18:05:33 +0700

    set up layout of calculator

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-calculator'>
+      <div className='cal-item-1'></div>
+      <div className='cal-item-2'></div>
+      <div className='cal-item-3'></div>
+      <div className='cal-item-4'></div>
+      <div className='cal-item-5'></div>
+      <div className='cal-item-6'></div>
+      <div className='cal-item-7'></div>
+      <div className='cal-item-8'></div>
+      <div className='cal-item-9'></div>
+      <div className='cal-item-10'></div>
+      <div className='cal-item-11'></div>
+      <div className='cal-item-12'></div>
+      <div className='cal-item-13'></div>
+      <div className='cal-item-14'></div>
+      <div className='cal-item-15'></div>
+      <div className='cal-item-16'></div>
+      <div className='cal-item-17'></div>
+      <div className='cal-item-18'></div>
+      <div className='cal-item-19'></div>
+      <div className='cal-item-20'></div>
     </div>
   );
 }
